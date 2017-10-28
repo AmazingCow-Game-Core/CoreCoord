4e2eca6e6a903e616a2d048d698befca3ba486f6
n2omatt <n2omatt@amazingcow.com>
2017-10-28 09:10:19 -0200

    - Cosmetic changes.

--- include/Coord.h
+++ include/Coord.h
@@ -52,7 +52,7 @@ public:
     static const Coord& Down();
 
 
-    // Operator //
+    // Operators //
 public:
     //Friends
     ///@brief Output a string representation in form of (y, x).
@@ -84,67 +84,95 @@ public:
     Coord& operator *=(int scalar);
 
 
-    // CTOR/DTOR //
+    // CTOR / DTOR //
 public:
-    ///@brief Constructs a Coord.
-    ///@param y The Y coordinate - Default is 0.
-    ///@param x The X coordinate - Default is 0.
-    ///@note Coord is Y axis first - Take care with it.
-    ///@warning Coord is Y axis first - Take care with it.
+    ///@brief
+    ///  Constructs a Coord.
+    ///@param y
+    ///  The Y coordinate - Default is 0.
+    ///@param x
+    ///  The X coordinate - Default is 0.
+    ///@note
+    ///  Coord is Y axis first - Take care with it.
+    ///@warning
+    ///  Coord is Y axis first - Take care with it.
     explicit Coord(int y = 0, int x = 0);
 
 
     // Public Methods //
 public:
-    ///@brief Gets a Coord that is top of this coord.
-    ///@param offset How many times it will be on top.
-    ///@returns A coord that have the y coordinate "offset" times less
-    ///than this coord.
+    ///@brief
+    ///  Gets a Coord that is top of this coord.
+    ///@param offset
+    ///  How many times it will be on top.
+    ///@returns
+    ///  A coord that have the y coordinate "offset" times less
+    ///  than this coord.
     Coord getUp(int offset = 1) const;
 
-    ///@brief Gets a Coord that is bottom of this coord.
-    ///@param offset How many times it will be on bottom.
-    ///@returns A coord that have the y coordinate "offset" times more
-    ///than this coord.
+    ///@brief
+    ///  Gets a Coord that is bottom of this coord.
+    ///@param
+    ///  offset How many times it will be on bottom.
+    ///@returns
+    ///  A coord that have the y coordinate "offset" times more
+    ///  than this coord.
     Coord getDown(int offset = 1) const;
 
-    ///@brief Gets a Coord that is on left of this coord.
-    ///@param offset How many times it will be on left.
-    ///@returns A coord that have the x coordinate "offset" times less
-    ///than this coord.
+    ///@brief
+    ///   Gets a Coord that is on left of this coord.
+    ///@param offset
+    ///   How many times it will be on left.
+    ///@returns
+    ///  A coord that have the x coordinate "offset" times less
+    ///  than this coord.
     Coord getLeft(int offset = 1) const;
 
-    ///@brief Gets a Coord that is on right of this coord.
-    ///@param offset How many times it will be on right.
-    ///@returns A coord that have the x coordinate "offset" times more
-    ///than this coord.
+    ///@brief
+    ///  Gets a Coord that is on right of this coord.
+    ///@param offset
+    ///  How many times it will be on right.
+    ///@returns
+    ///  A coord that have the x coordinate "offset" times more
+    ///  than this coord.
     Coord getRight(int offset = 1) const;
 
-    ///@brief Gets a Coord that is in the middle of two coords.
-    ///@param coord2 The second coord that will be compared.
-    ///@returns A coord that has the y and y coordinates specified
-    ///by formula: (c1.y + c2.y) / 2 and (c1.x + c2.x) / 2.
+    ///@brief
+    ////  Gets a Coord that is in the middle of two coords.
+    ///@param coord2
+    ///   The second coord that will be compared.
+    ///@returns
+    ///   A coord that has the y and y coordinates specified
+    ///   by formula: (c1.y + c2.y) / 2 and (c1.x + c2.x) / 2.
     Coord getMiddle(const Coord &coord2) const;
 
 
-    ///@brief Gets a the orthogonal coords.
-    ///@returns a vector of coords starting from top going clockwise.
+    ///@brief
+    ///  Gets a the orthogonal coords.
+    ///@returns
+    ///  a vector of coords starting from top going clockwise.
     ///@see Coord::getSurrounding.
     Vec getOrthogonal() const;
 
-    ///@brief Gets a the surrounding coords.
-    ///@returns a vector of coords starting from top going clockwise.
+    ///@brief
+    ///  Gets a the surrounding coords.
+    ///@returns
+    //// a vector of coords starting from top going clockwise.
     ///@see Coord::getSurrounding.
     Vec getSurrounding() const;
 
 
-    ///@brief Gets if the both coords have the same X coordinate.
-    ///@returns True if they are at same X, false otherwise.
+    ///@brief
+    ///  Gets if the both coords have the same X coordinate.
+    ///@returns
+    ///  True if they are at same X, false otherwise.
     ///@see isSameY().
     bool isSameX(const Coord &coord2) const;
 
-    ///@brief Gets if the both coords have the same Y coordinate.
-    ///@returns True if they are at same Y, false otherwise.
+    ///@brief
+    ///  Gets if the both coords have the same Y coordinate.
+    ///@returns
+    ///  True if they are at same Y, false otherwise.
     ///@see isSameX().
     bool isSameY(const Coord &coord2) const;
 
